02aa2fef40e7247a92480d52343b4f8dff1f5a4c
carol-sf <annacarolina.franca21@gmail.com>
2023-04-09 10:31:51 -0300

    adicionando as classes Dev e Bootcamp

---
+++ src/Bootcamp.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicial;
+    private LocalDate dataFinal;
+    private Set<Dev> devsInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public Bootcamp() {
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+    public void setDataInicial(LocalDate dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+    public void setDataFinal(LocalDate dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+}
---
+++ src/Dev.java
@@ -0,0 +1,58 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public Dev() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+    
+    public void inscreverNoBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.out.println("Você não está inscrito em nenhum conteudo.");
+        }
+    }
+
+    public double calcularTotalXP() {
+        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
+        double soma = 0;
+        while(iterator.hasNext()) {
+            double next = iterator.next().calcularXP();
+            soma += next;
+        }
+        return soma;
+    }
+}
